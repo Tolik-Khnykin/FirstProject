4af906cb91d62d4c348e25e5e5b84a61b2cace83
Tolik-Khnykin <anatolijhnykin726@gmail.com>
2023-04-30 21:03:33 +0300

    otrezalYaycaPrimatu

---
+++ index.js
@@ -0,0 +1,15 @@
+const sdelalPirsingZahary = {
+    otrezalYaycaPrimatu: true,
+    yaycaKuky(yaycaNosoroga, pursingMneVKedy) {
+        if (pursingMneVKedy === true) {
+            console.log(yaycaNosoroga);
+        } else {
+            console.log('simenPasheVRot')
+        }
+    }
+}
+
+sdelalPirsingZahary.yaycaKuky(400000000000, false)
+sdelalPirsingZahary.yaycaKuky(23 ** 3, false)
+sdelalPirsingZahary.yaycaKuky(666 ** 2, true)
+console.log(sdelalPirsingZahary.otrezalYaycaPrimatu)
